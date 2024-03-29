33bb32a57530d3cca859619400058f489fad3b5e
Safayat <safayathossain40@gmail.com>
2024-03-29 14:37:40 +0600

    added

---
+++ callApply.js
@@ -0,0 +1,36 @@
+const normalPerson ={
+    firstName : 'Rahim',
+    lastName : 'Uddin',
+    salary: 15000,
+    getFullName: function(){
+        console.log(this.firstName+" "+this.lastName);
+    },
+    chargeBill:  function(amount){
+        console.log(this);
+        this.salary = this.salary - amount;
+        return this.salary;
+    }
+}
+
+const heroPerson = {
+    firstName : 'Hero',
+    lastName : 'Balam',
+    salary : 25000
+}
+
+const firendlyPerson = {
+    firstName : 'Hero',
+    lastName : 'golam',
+    salary : 25000
+}
+
+//normalPerson.chargeBill();
+const heroBillCharge  = normalPerson.chargeBill.bind(heroPerson)
+heroBillCharge(2000);
+heroBillCharge
+
+
+
+// normalPerson.chargeBill(150);
+// normalPerson.chargeBill(3000);
+// console.log(normalPerson.salary);
